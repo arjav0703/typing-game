17a38ec88d241e7252005632f46139d58f39c926
Arjav Jain <arjavjain0703@gmail.com>
2025-08-10 02:02:10 -0400

    v0.1: got the basic stuff working

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use futures::{SinkExt, StreamExt};
+use tokio::io::{self, AsyncBufReadExt};
+use tokio_tungstenite::connect_async;
+
+#[tokio::main]
+async fn main() {
+    let url = "ws://127.0.0.1:9001";
+    let (ws_stream, _) = connect_async(url).await.unwrap();
+    let (mut write, mut read) = ws_stream.split();
+
+    // Task to receive sentence updates
+    let read_task = tokio::spawn(async move {
+        while let Some(Ok(msg)) = read.next().await {
+            println!("\nCurrent Sentence: {}", msg.to_text().unwrap_or(""));
+            print!("Your letter: ");
+            use std::io::Write;
+            std::io::stdout().flush().unwrap();
+        }
+    });
+
+    // Read one letter at a time from stdin
+    let stdin = io::BufReader::new(io::stdin());
+    let mut lines = stdin.lines();
+
+    while let Ok(Some(line)) = lines.next_line().await {
+        if let Some(c) = line.chars().next() {
+            let _ = write.send(c.to_string().into()).await;
+        }
+        print!("Your letter: ");
+        use std::io::Write;
+        std::io::stdout().flush().unwrap();
+    }
+
+    read_task.abort();
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,49 @@
-fn main() {
-    println!("Hello, world!");
+use futures::{SinkExt, StreamExt};
+use std::sync::{Arc, Mutex};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::sync::broadcast;
+use tokio_tungstenite::accept_async;
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:9001").await.unwrap();
+    println!("Server running on ws://127.0.0.1:9001");
+
+    let sentence = Arc::new(Mutex::new(String::new()));
+    let (tx, _rx) = broadcast::channel(100);
+
+    while let Ok((stream, _)) = listener.accept().await {
+        let sentence = Arc::clone(&sentence);
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move {
+            let ws_stream = accept_async(stream).await.unwrap();
+            let (mut write, mut read) = ws_stream.split();
+
+            // Send current sentence to new client
+            {
+                let current = sentence.lock().unwrap().clone();
+                let _ = write.send(current.clone().into()).await;
+            }
+
+            loop {
+                tokio::select! {
+                    Some(Ok(msg)) = read.next() => {
+                        let msg_text = msg.to_text().unwrap_or("");
+                        if msg_text.len() == 1 {
+                            let mut s = sentence.lock().unwrap();
+                            s.push_str(msg_text);
+                            println!("Updated sentence: {}", s);
+                            let _ = tx.send(s.clone());
+                        }
+                    }
+
+                    Ok(msg) = rx.recv() => {
+                        let _ = write.send(msg.into()).await;
+                    }
+                }
+            }
+        });
+    }
 }
